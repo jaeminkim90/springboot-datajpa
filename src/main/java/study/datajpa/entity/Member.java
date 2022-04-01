daacc1ee3ea5e10cf8ea8ab4fab38a17a871ccf0
Jaemin Kim <jaeminkim90@gmail.com>
2022-04-01 11:13:05 +0900

    test: 엔티티매니저를 이용해 데이터를 저장하고 불러오는 테스트 구현

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -18,5 +18,18 @@ public class Member {
 	private String username;
 
 
+	// JPA는 기본 생성자가 필요하다
+	// JPA 표준 스펙은 default 생성자가 하나 필요하다
+	protected Member() {
+	}
 
+	// 생성자 방식의 주입은 setter를 없앨 수 있다
+	public Member(String username) {
+		this.username = username;
+	}
+
+	// 나중에 이름 변경이 필요하면 changeUsername() 같은 메서드를 만들어 사용한다
+	public void changeUsername(String username) {
+		this.username = username;
+	}
 }
--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -1,7 +1,38 @@
 package study.datajpa.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
 class MemberJpaRepositoryTest {
+
+	@Autowired
+	MemberJpaRepository memberJpaRepository;
+
+
+	@Test
+	void testmember() throws Exception{
+
+		// 저장
+		Member member = new Member("memberA");
+		Member savedmember = memberJpaRepository.save(member);
+
+		// 조회
+		Member findMember = memberJpaRepository.find(savedmember.getId());
+
+		// 검증
+		assertThat(findMember.getId()).isEqualTo(member.getId());
+		assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+		assertThat(findMember).isEqualTo(member);
+	}
 
 }
