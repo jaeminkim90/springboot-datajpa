51f2faeb931732b27d7a7d4637bbb8340650d1c2
Jaemin Kim <jaeminkim90@gmail.com>
2022-04-01 13:24:01 +0900

    feat: 연관관계 세팅 메서드 구현

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,17 +1,21 @@
 package study.datajpa.entity;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA 스펙은 기본 생성자를 필요로 한다
+@ToString(of = {"id", "usename", "age"}) // 객체를 찍을 때 정보를 바로 출력한다
 public class Member {
 
 	@Id
@@ -26,13 +30,23 @@ public class Member {
 	private Team team;
 
 	// JPA는 기본 생성자가 필요하다
-	// JPA 표준 스펙은 default 생성자가 하나 필요하다
-	protected Member() {
-	}
+	// JPA 표준 스펙은 default 생성자가 하나 필요하다. private AccessLevel은 사용 불가하다.
+	// 간단하게 NoArgsConstruct를 사용해도 된다
+//	protected Member() {
+//	}
 
 	// 생성자 방식의 주입은 setter를 없앨 수 있다
 	public Member(String username) {
 		this.username = username;
+	}
+
+	// 서로 연관관계를 세팅해주는 메서드가 필요하다
+	// Team을 변경할 때 사용
+	public void changeTeam(Team team) {
+		this.team = team; // 파라미터로 들어온 team을 넣는다
+		team.getMembers().add(this); // 변경된 팀에도 List(Members)에 현재의 Member 자기 자신을 추가한다
+
+
 	}
 
 	// 나중에 이름 변경이 필요하면 changeUsername() 같은 메서드를 만들어 사용한다
--- src/main/java/study/datajpa/entity/Team.java
+++ src/main/java/study/datajpa/entity/Team.java
@@ -8,8 +8,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
 public class Team {
 
 	@Id
@@ -18,10 +27,10 @@ public class Team {
 	private Long id;
 	private String name;
 
-	@OneToMany(mappedBy ="team") // 1:多 관계에서 mapped by는 FK가 없는 쪽에 걸어주는 것이 좋다
+	@OneToMany(mappedBy = "team") // 1:多 관계에서 mapped by는 FK가 없는 쪽에 걸어주는 것이 좋다
 	private List<Member> members = new ArrayList<>();
 
-
-
-
+	public Team(String name) {
+		this.name = name;
+	}
 }
