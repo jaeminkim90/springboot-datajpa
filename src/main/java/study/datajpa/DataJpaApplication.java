6baecad7fb2d3edf62a92e1ac37e0cb16c64d715
Jaemin Kim <jaeminkim90@gmail.com>
2022-04-08 17:16:12 +0900

    feat: 시간 관련 데이터와 이름 관련 데이터를 나눠서 클래스 구현

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -1,10 +1,16 @@
 package study.datajpa;
 
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//(modifyOnCreate = false)을 추가로 설정하면 업데이트는 null이 들어간다
 @EnableJpaAuditing // Spring Data JPA를 이용하여 BaseEntity룰 사용할 경우 필요한 애노테이션
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "study.datajpa.repository") // 스프링 부트를 쓰면 없어도 된다
@@ -12,6 +18,20 @@ public class DataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataJpaApplication.class, args);
+	}
+
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString());
+
+//		return new AuditorAware<String>() {
+//
+//			// interface에서 메서드가 하나이면 람다로 변경 가능
+//			@Override
+//			public Optional<String> getCurrentAuditor() {
+//				return Optional.of(UUID.randomUUID().toString());
+//			}
+//		};
 	}
 }
 
--- src/main/java/study/datajpa/entity/BaseTimeEntity.java
+++ src/main/java/study/datajpa/entity/BaseTimeEntity.java
@@ -5,22 +5,20 @@ import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 
 @EntityListeners(AuditingEntityListener.class) // Spring Data JPA 방식을 사용한다면 필요한 애노테이션이다
 @MappedSuperclass
 @Getter
-public class BaseEntity extends BaseTimeEntity{
+public class BaseTimeEntity {
 
-	@CreatedBy
-	@Column(updatable = false) // 등록자는 수정되지 않도록 처리하는 것이 좋다
-	private String createdBy;
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdDate;
 
-	@LastModifiedBy
-	private String lastModifiedBy;
+	@LastModifiedDate
+	private LocalDateTime lastModifiedDate;
+
 }
