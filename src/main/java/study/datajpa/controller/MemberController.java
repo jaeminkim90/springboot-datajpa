b67e4880ca820ee3aa63c7389d7e6180eb1ac1d5
Jaemin Kim <jaeminkim90@gmail.com>
2022-04-09 17:47:12 +0900

    feat: page 정보를 파라미터로 받아 페이징 처리된 Member를 반환

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,6 +2,8 @@ package study.datajpa.controller;
 
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,14 +24,26 @@ public class MemberController {
 
 	@GetMapping("/members2/{id}")
 	public String findMember(@PathVariable("id") Member member) {
-		// 컨버팅 기능스: 스프링이 파라미터로 들어온 id를 이용해서 아래 주석에 해당하는 member 조회 기능을 자동으로 수행한다.
+		// 컨버터 기능: 스프링이 파라미터로 들어온 id를 이용해서 아래 주석에 해당하는 member 조회 기능을 자동으로 수행한다.
 		// Member member = memberRepository.findById(id).get();
 
 		return member.getUsername();
+	}
+
+	@GetMapping("/members")
+	public Page<Member> list(Pageable pageable) {
+		Page<Member> page = memberRepository.findAll(pageable);
+		return page;
+	}
+
+
+
 
 	@PostConstruct
 	public void init() {
-		memberRepository.save(new Member("userA"));
-
+		for (int i = 0; i < 100; i++) {
+			memberRepository.save(new Member("user" + i, i));
+		}
 	}
 }
+
