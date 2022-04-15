491802fc703cd51deb4e2d3cdd8fc91d90195270
Jaemin Kim <jaeminkim90@gmail.com>
2022-04-16 07:00:25 +0900

    feat: Persistable 인터페이스를 이용하여 CreatedDate 생성 여부로 isNew 엔티티인지 확인 가능

--- src/main/java/study/datajpa/entity/Item.java
+++ src/main/java/study/datajpa/entity/Item.java
@@ -1,23 +1,38 @@
 package study.datajpa.entity;
 
+import java.time.LocalDateTime;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.cache.internal.StandardTimestampsCacheFactory;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-@Getter
+@EntityListeners(AuditingEntityListener.class) // @createdDate 사용을 위해 필요하다
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Item {
+public class Item implements Persistable<String> {
 
 	@Id
-	@GeneratedValue
 	private String id;
+
+	@CreatedDate // JPA 이벤트로 persist 후에 호출된다. isNew를 판단하는데 사용할 수 있다.
+	private LocalDateTime createdDate;
 
 	public Item(String id) {
 		this.id = id;
+	}
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public boolean isNew() { // createdDate를 기준으로 새로운 엔티티 여부를 확인 할 수 있다.
+		return createdDate == null;
 	}
 }
--- src/test/java/study/datajpa/repository/ItemRepositoryTest.java
+++ src/test/java/study/datajpa/repository/ItemRepositoryTest.java
@@ -17,7 +17,10 @@ class ItemRepositoryTest {
 	@Test
 	public void save() {
 		Item item = new Item("A");
-		itemRepository.save(item); // Item의 Id를 따로 넣지 않아도 JPA persist가 완료되면 그 안에서 들어간다
+		itemRepository.save(item);
+		// Item의 Id를 따로 넣지 않아도 JPA persist가 완료되면 그 안에서 들어간다.
+		// 만약 Id를 임의로 넣어주면 DB에 있는 데이터라고 생각해서 바로 select 쿼리가 나간다.
+		// 그때 데이터가 없을 경우 다시 insert 쿼리가 나간다.
 
 	}
 }
