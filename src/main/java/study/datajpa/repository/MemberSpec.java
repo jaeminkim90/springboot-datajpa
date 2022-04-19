d9210ee0f4f93c9e4d19fd84606be5cea21dbd99
Jaemin Kim <jaeminkim90@gmail.com>
2022-04-19 20:40:19 +0900

    feat: Specification를 람다 형식으로 구현

--- src/main/java/study/datajpa/repository/MemberSpec.java
+++ src/main/java/study/datajpa/repository/MemberSpec.java
@@ -2,19 +2,25 @@ package study.datajpa.repository;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 public class MemberSpec {
 
 	public static Specification<Member> teamName(final String teamName) {
-		return new Specification<Member>() {
-			@Override
-			public Predicate toPredicate(Root<Member> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+		return (root, query, criteriaBuilder) -> {
+
+			if(StringUtils.isEmpty(teamName)) {
 				return null;
 			}
+			Join<Member, Team> t = root.join("team", JoinType.INNER);// 회원과 조인
+			return criteriaBuilder.equal(t.get("name"), teamName);
 		};
 	}
 }
